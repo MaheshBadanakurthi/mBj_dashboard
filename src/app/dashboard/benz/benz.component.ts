90f18e8f79a0b77add1463d6cd47f614e187251b
Mahesh <itstribe.mahi@gmail.com>
2023-03-15 19:40:29 +0530

    Location updated

--- src/app/dashboard/benz/benz.component.ts
+++ src/app/dashboard/benz/benz.component.ts
@@ -1,5 +1,7 @@
-import {  Component, OnInit, ViewChild } from '@angular/core';
-import {MapGeocoder } from '@angular/google-maps'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapGeocoder } from '@angular/google-maps';
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-benz',
@@ -7,7 +9,7 @@ import {MapGeocoder } from '@angular/google-maps'
   styleUrls: ['./benz.component.scss'],
 })
 export class BenzComponent implements OnInit {
-  constructor( public geoCoder:MapGeocoder ) {}
+  constructor(public geoCoder: MapGeocoder) {}
 
   public current_date = new Date();
   public imgUrl!: any;
@@ -19,14 +21,17 @@ export class BenzComponent implements OnInit {
   public center: google.maps.LatLngLiteral = { lat: 0, lng: 0 };
   public address: string = '';
   public google: any;
+  public show: boolean = false;
 
   ngOnInit(): void {}
+
   public imgUpload(event: any): void {
     const imageFile = event.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(imageFile);
     reader.onload = () => {
       this.imgUrl = reader.result;
+      this.show = true;
     };
     this.latLng();
     this.deviceId = this.deviceId = window.navigator.userAgent;
@@ -36,21 +41,42 @@ export class BenzComponent implements OnInit {
     navigator.geolocation.getCurrentPosition((position) => {
       this.latitude = position.coords.latitude;
       this.longitude = position.coords.longitude;
-      console.log(this.latitude);
-      console.log(this.longitude);
-      this.center = { lat: this.latitude, lng: this.longitude }; // Set actual values for lat and lng
-      console.log(this.center);
-      this.getAddress()
+
+      this.center = { lat: this.latitude, lng: this.longitude };
     });
   }
 
- public getAddress():void{
-  this.geoCoder.geocode({
-    address:"Madhapur hyderabad "
-  }).subscribe((result)=>{
-    console.log(result);
+  public imgForLoc: any;
+  public latt!: number;
+  public longit!: number;
+  map!: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
 
-  })
- }
+  public imageUploaded(event: any): void {
+    const imgFile = event.target.files[0];
+    console.log(imgFile);
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(imgFile);
+    fileReader.onload = () => {
+      this.imgForLoc = fileReader.result;
+    };
 
+    mapboxgl as typeof mapboxgl;
+    this.map = new mapboxgl.Map({
+      accessToken:'pk.eyJ1IjoiZHBpZXRyb2NhcmxvIiwiYSI6ImNram9tOGFuMTBvb3oyeXFsdW5uYmJjNGQifQ._zE6Mub0-Vpl7ggMj8xSUQ',
+       container:'map',
+       style: this.style,
+      zoom: 16,
+    })
+    this.map.addControl(new mapboxgl.NavigationControl());
+    new MapboxGeocoder({
+      accessToken:mapboxgl.accessToken,
+      mapboxgl:mapboxgl
+    })
+    navigator.geolocation.getCurrentPosition((data) => {
+      this.latt = data.coords.latitude;
+      this.longit = data.coords.longitude;
+      new mapboxgl.Marker().setLngLat([this.longitude, this.latitude]).addTo(this.map)
+    });
+  }
 }
--- src/app/dashboard/bugatti/bugatti.component.ts
+++ src/app/dashboard/bugatti/bugatti.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 
 @Component({
   selector: 'app-bugatti',
@@ -6,55 +8,41 @@ import { Component, ElementRef, ViewChild, AfterViewInit, OnInit } from '@angula
   styleUrls: ['./bugatti.component.scss'],
 })
 export class BugattiComponent implements OnInit {
-  @ViewChild('videoElm', { static: true })
-  videoElm!: ElementRef<HTMLVideoElement>;
-  @ViewChild('canvasElm', { static: true })
-  canvasElm!: ElementRef<HTMLCanvasElement>;
- public watermarkText:string="SEBI";
- public isClicked = false;
 
+  map!: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+
+  public lat!:number
+  public lng!:number
   constructor() {}
 
   ngOnInit(): void {
-    this.waterMark();
-  }
-
-
-
-  public waterMark() {
-    const video = this.videoElm.nativeElement;
-    const canvas = this.canvasElm.nativeElement;
-    const ctx = canvas.getContext('2d');
-
-    video.addEventListener('play', () => {
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      if (ctx) {
-        const drawFrame = () => {
-          if (!video.paused && !video.ended) {
-            // draw the original video frame
-            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-            // draw the watermark text on top of the original video frame
-            ctx.font = 'bold 24px Arial';
-            ctx.fillStyle = 'red';
-            ctx.textAlign = 'left';
-            ctx.textBaseline = 'top';
-            ctx.fillText('Watermark Text', 50, 50);
-            // render the frame to the canvas
-            requestAnimationFrame(drawFrame);
-          }
-        };
-        drawFrame();
-
-        // add click event listener to video element
-        video.addEventListener('click', () => {
-          // remove the event listener to prevent creating a new video element
-          video.removeEventListener('click', () => {});
-          // start playing the video
-          video.play();
-        });
-      }
+    mapboxgl as typeof mapboxgl;
+    this.map = new mapboxgl.Map({
+      accessToken:
+        'pk.eyJ1IjoiZHBpZXRyb2NhcmxvIiwiYSI6ImNram9tOGFuMTBvb3oyeXFsdW5uYmJjNGQifQ._zE6Mub0-Vpl7ggMj8xSUQ',
+      container: 'map',
+      style: this.style,
+      zoom: 16,
+      // center: [this.lng, this.lat],
     });
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+      // Add geocoder control
+  const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+  });
+
+  // Get current location
+  navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+   this.map.setCenter([longitude, latitude]);
+   console.log({ latitude, longitude });
+   const marker = new mapboxgl.Marker()
+   .setLngLat([longitude, latitude])
+   .addTo(this.map);
+  });
   }
 
 
